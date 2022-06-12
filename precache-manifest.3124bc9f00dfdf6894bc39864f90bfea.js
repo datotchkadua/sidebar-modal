ed1aad2743c28ff3686ac9f97ccc3036b0a2d81c
dato <dato.tchkadua.1@iliauni.edu.ge>
2022-06-12 20:04:55 +0400

    Updates

---
+++ precache-manifest.3124bc9f00dfdf6894bc39864f90bfea.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cfbea17cd66209a389766ea7eeea5658",
+    "url": "/sidebar-modal/index.html"
+  },
+  {
+    "revision": "f046c2114728b70a766c",
+    "url": "/sidebar-modal/static/css/main.e0f79945.chunk.css"
+  },
+  {
+    "revision": "88953e1dc5e7da9cfe5c",
+    "url": "/sidebar-modal/static/js/2.97e84563.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/sidebar-modal/static/js/2.97e84563.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f046c2114728b70a766c",
+    "url": "/sidebar-modal/static/js/main.ee9fc21c.chunk.js"
+  },
+  {
+    "revision": "a8cd427f675fc95d7c22",
+    "url": "/sidebar-modal/static/js/runtime-main.328f120b.js"
+  },
+  {
+    "revision": "2bb7da653fb63433560ef10511c9ae9d",
+    "url": "/sidebar-modal/static/media/logo.2bb7da65.svg"
+  }
+]);
